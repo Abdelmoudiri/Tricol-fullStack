ba6f71cd063cf5f2a4f1d233394fab49bd4e8de4
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-01 12:26:03 +0100

    ajouter entity commandeFournisseur

--- src/main/java/com/tricol/tricolV2/entity/CommandeFournisseur.java
+++ src/main/java/com/tricol/tricolV2/entity/CommandeFournisseur.java
@@ -2,26 +2,46 @@ package com.tricol.tricolV2.entity;
 
 import com.tricol.tricolV2.entity.enums.StatutCommande;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommandeFournisseur {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull(message = "La date de commande est obligatoire")
+    @PastOrPresent(message = "La date de commande doit être dans le passé ou aujourd’hui")
     private LocalDateTime dateCommande;
+
+    @NotNull(message = "Le statut de la commande est obligatoire")
+    @Enumerated(EnumType.STRING)
     private StatutCommande statut;
+
+    @NotNull(message = "Le montant total est obligatoire")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Le montant total doit être supérieur à zéro")
     private BigDecimal montantTotal;
 
     @ManyToOne
-    @JoinColumn(name = "fournisseur_id")
+    @JoinColumn(name = "fournisseur_id", nullable = false)
+    @NotNull(message = "Le fournisseur est obligatoire")
     private Fournisseur fournisseur;
 
-    @OneToMany(mappedBy = "commandeFournisseur", cascade = CascadeType.ALL)
-    private List<LigneCommandeFournisseur>lignes;
-    private List <MouvementStock>mouvementsStock;
+    @OneToMany(mappedBy = "commandeFournisseur", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LigneCommandeFournisseur> lignes;
 
+    @OneToMany(mappedBy = "commandeFournisseur", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MouvementStock> mouvementsStock;
 }
