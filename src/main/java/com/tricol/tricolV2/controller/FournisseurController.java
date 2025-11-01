c2c0884bc2c6964b07d0d9e01f280146ccb25773
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-01 17:16:07 +0100

    ajouter function get fournisseur by id  dans controller

--- src/main/java/com/tricol/tricolV2/controller/FournisseurController.java
+++ src/main/java/com/tricol/tricolV2/controller/FournisseurController.java
@@ -1,4 +1,29 @@
 package com.tricol.tricolV2.controller;
 
+import com.tricol.tricolV2.dto.FournisseurDTO;
+import com.tricol.tricolV2.exception.NotFoundException;
+import com.tricol.tricolV2.service.FournisseurServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v2/fournisseurs")
 public class FournisseurController {
+
+    private final FournisseurServiceImpl fournisseurService;
+    public FournisseurController(FournisseurServiceImpl fournisseurService){
+        this.fournisseurService =fournisseurService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FournisseurDTO>getFournisseurById(@PathVariable("id") Long id){
+       FournisseurDTO dto = fournisseurService.getFournisseurById(id)
+               .orElseThrow(()->new NotFoundException("Fournisseur non trouvé avec l'id : " + id));
+        return ResponseEntity.ok(dto);
+    }
+
 }
--- src/main/java/com/tricol/tricolV2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tricol/tricolV2/exception/GlobalExceptionHandler.java
@@ -2,15 +2,24 @@ package com.tricol.tricolV2.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
-    }
+    public ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
+        body.put("message", ex.getMessage());
 
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
 }
