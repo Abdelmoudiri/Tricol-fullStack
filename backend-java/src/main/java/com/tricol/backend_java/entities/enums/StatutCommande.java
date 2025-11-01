e3ff77084daca89cfe62822881b0de172b99e753
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-01 09:26:53 +0100

    Add StatutCommande and TypeMouvement enums for order status and movement types

---
+++ backend-java/src/main/java/com/tricol/backend_java/entities/enums/StatutCommande.java
@@ -0,0 +1,9 @@
+package com.tricol.backend_java.entities.enums;
+
+
+public enum StatutCommande {
+    EN_ATTENTE,
+    VALIDEE,
+    LIVREE,
+    ANNULEE
+}
---
+++ backend-java/src/main/java/com/tricol/backend_java/entities/enums/TypeMouvement.java
@@ -0,0 +1,8 @@
+package com.tricol.backend_java.entities.enums;
+
+
+public enum TypeMouvement {
+    ENTREE,
+    SORTIE,
+    AJUSTEMENT
+}
