7241e3235bc32293110e2e90e9db501a49d76092
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-01 09:24:03 +0100

    Add ProduitRepository interface for CRUD operations on Produit entity

---
+++ backend-java/src/main/java/com/tricol/backend_java/repositories/ProduitRepository.java
@@ -0,0 +1,9 @@
+package com.tricol.backend_java.repositories;
+
+import com.tricol.backend_java.entities.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+}
