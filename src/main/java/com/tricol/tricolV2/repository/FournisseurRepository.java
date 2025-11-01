f106bdd2bc297d8f73b77993b0d941e7e506fd7d
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-01 14:53:50 +0100

    ajouter repositoy de fournisseur

--- src/main/java/com/tricol/tricolV2/repository/FournisseurRepository.java
+++ src/main/java/com/tricol/tricolV2/repository/FournisseurRepository.java
@@ -1,4 +1,18 @@
 package com.tricol.tricolV2.repository;
 
-public class FournisseurRepository {
+import com.tricol.tricolV2.entity.Fournisseur;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FournisseurRepository extends JpaRepository<Fournisseur ,Long> {
+
+    List<Fournisseur> findBySocieteContainingIgnoreCase(String societe);
+    List<Fournisseur> findByVille(String ville);
+    Optional<Fournisseur> findByIce(String ice);
+
 }
