067ed81191544fefbf5a68343bf5ddae124c3cc3
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-04 16:20:14 +0100

    j'ai ajouter movementstockDTO

--- src/main/java/com/tricol/tricolV2/dto/MouvementStockDTO.java
+++ src/main/java/com/tricol/tricolV2/dto/MouvementStockDTO.java
@@ -1,4 +1,25 @@
 package com.tricol.tricolV2.dto;
 
+import com.tricol.tricolV2.entity.enums.TypeMouvement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MouvementStockDTO {
+    private Long id;
+    private LocalDateTime dateMouvement;
+    private TypeMouvement type;
+    private BigDecimal quantite;
+    private BigDecimal coutUnitaire;
+    private Long produitId;
+    private Long commandeId;
+    private String commentaire;
 }
