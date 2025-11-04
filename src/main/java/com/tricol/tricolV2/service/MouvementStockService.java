8a5749db8498a9fd76457b2b8f7f094deba2eb55
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-11-04 16:22:45 +0100

    j'ai ajoute movment stockService

--- src/main/java/com/tricol/tricolV2/service/MouvementStockService.java
+++ src/main/java/com/tricol/tricolV2/service/MouvementStockService.java
@@ -1,4 +1,17 @@
 package com.tricol.tricolV2.service;
 
-public class MouvementStockService {
+import com.tricol.tricolV2.dto.MouvementStockDTO;
+import com.tricol.tricolV2.entity.CommandeFournisseur;
+import com.tricol.tricolV2.entity.enums.TypeMouvement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface MouvementStockService {
+    void createEntriesForCommande(CommandeFournisseur commande);
+    boolean movementsExistForCommande(Long commandeId);
+
+    Page<MouvementStockDTO> getPaged(Pageable pageable);
+    Page<MouvementStockDTO> getByProduit(Long produitId, Pageable pageable);
+    Page<MouvementStockDTO> getByCommande(Long commandeId, Pageable pageable);
+    Page<MouvementStockDTO> getByType(TypeMouvement type, Pageable pageable);
 }
