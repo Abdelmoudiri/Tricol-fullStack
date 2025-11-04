f7e5287221e2f37653766276bfeeb455713ea491
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-04 15:12:47 +0100

    Add missing import statement for List in FournisseurService

--- backend-java/src/main/java/com/tricol/backend_java/services/FournisseurService.java
+++ backend-java/src/main/java/com/tricol/backend_java/services/FournisseurService.java
@@ -5,10 +5,14 @@ import com.tricol.backend_java.dto.FournisseurDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface FournisseurService {
     FournisseurDTO create(FournisseurDTO dto);
     FournisseurDTO update(Long id, FournisseurDTO dto);
     void delete(Long id);
     FournisseurDTO getById(Long id);
     Page<FournisseurDTO> getAll(Pageable pageable);
+
+    Boolean existsByEmail(String email);
 }
