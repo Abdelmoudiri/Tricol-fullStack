03d1fd36a9aac8359dd41f5a7e839854072f61fb
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-04 15:12:32 +0100

    Add import statements for FournisseurDTO and List in FournisseurRepository

--- backend-java/src/main/java/com/tricol/backend_java/repositories/FournisseurRepository.java
+++ backend-java/src/main/java/com/tricol/backend_java/repositories/FournisseurRepository.java
@@ -1,9 +1,14 @@
 package com.tricol.backend_java.repositories;
 
+import com.tricol.backend_java.dto.FournisseurDTO;
 import com.tricol.backend_java.entities.Fournisseur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
+
+    Boolean existsByEmail(String email);
 }
