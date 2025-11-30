eaa7f550061dfbbdda6c5569037befd75bc1aa70
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-11-30 09:19:47 +0100

    Add FournisseurRepository interface for data access

---
+++ backend-java/src/main/java/com/tricol/backend_java/repositories/FournisseurRepository.java
@@ -0,0 +1,9 @@
+package com.tricol.backend_java.repositories;
+
+import com.tricol.backend_java.entities.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
+}
